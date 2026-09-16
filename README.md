518ddc2906d05e88019273162616a3b5d9032507
brodskym <marekbrodsky44@gmail.com>
2026-09-16 20:17:19 +0200

    Langevian thermostat and BAOAB algorithm for many particle system

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Molecular Dynamics I Final Project
 ==================================
 
-This code is application of molecular dynamics knowledge to "Particle life" system.
+This code is application of molecular dynamics knowledge to "Particle life" system. The chosen molecular dynamics is Langevinian with BAOAB algorithm.
 
 Before running you need to set in terminal 'QT_QPA_PLATFORM=wayland'.
--- main.py
+++ main.py
@@ -4,28 +4,44 @@ def force_function(r):
     k = 1.5
     return -k * r
 
-# Initial conditions
+def random_fuction(n):
+    return np.random.normal(0,1.0,size=(n,2))
 
-position = np.array([1.0, 1.0], dtype=np.float64)
-velocity = np.array([0.0, 0.0], dtype=np.float64)
+# Plane dimension
+
+plane_size = 10.0
+N = 100 # number of particles
 
 # Time axis settings
 
-time_max = 10.0
 dt = 1.0  
-steps = int(time_max / dt)
+steps = 100
 
+# Initial conditions
+
+position = np.random.uniform(low=-plane_size, high=plane_size, size=(N, 2))
+momentum = np.zeros((N,2), dtype=np.float64)
 force = force_function(position)
-velocity += 0.5 * force * dt
+m = 1.0
+gamma = 1.0
+k_B = 1.0 # Boltzman constant in SI k_B = 1.380649e-23 J/K
+T = 1.0
+
+A = np.exp(-gamma * dt)
+B = np.sqrt((1-A**2) * k_B * T * m)
 
 # Main loop
 
 for step in range(steps):
-    position += velocity * dt
+    momentum += 0.5*force*dt
+    position += 0.5*momentum/m*dt
+    momentum *= A
+    momentum += B*random_fuction(N)
+    position += 0.5*momentum/m*dt
     force = force_function(position)
-    velocity += force * dt
+    momentum += 0.5*force*dt
 
     if step % 2 == 0:
         print(f"t = {step * dt:.2f}")
         print(f"Position: {position}")
-        print(f"Velocity: {velocity}\n")
+        print(f"Momentum: {momentum}\n")
