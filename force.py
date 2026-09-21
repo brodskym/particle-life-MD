PBC and corrected distance calculation

force.py
@@ -1,13 +1,17 @@
 import numpy as np
 
-def force_function(positions, types, force_matrix, r_c):
+def force_function(positions, types, force_matrix, r_c, grid_size):
     N = positions.shape[0]
     total_forces = np.zeros((N,2), dtype=np.float64)
 
     for i in range(N):
         for j in range(i):
             dx = positions[j][0]-positions[i][0]
+            dx = dx - grid_size*round(dx/grid_size) # Correction according to PBC
+
             dy = positions[j][1]-positions[i][1]
+            dy = dy - grid_size*round(dy/grid_size) # Correction according to PBC
+
             distance = np.sqrt(dx**2 + dy**2)
 
             if 0 < distance < r_c:

main.py
@@ -4,9 +4,11 @@ from force import force_function
 def random_fuction(n):
     return np.random.normal(0,1.0,size=(n,2))
 
-# Plane dimension
+# Grid dimension
 
-plane_size = 10.0
+cut_off = 1.5
+no_of_cells = 10
+grid_size = no_of_cells * cut_off
 N = 10 # number of particles
 
 # Time axis settings
@@ -16,16 +18,21 @@ steps = 100
 
 # Initial conditions - particles
 
-position = np.random.uniform(low=-plane_size, high=plane_size, size=(N, 2))
+position = np.random.uniform(low=-grid_size/2, high=grid_size/2, size=(N, 2))
 momentum = np.zeros((N,2), dtype=np.float64)
 type_of_particle = np.zeros((N,), dtype=np.int32)
+
+
 
 # Initial conditions - forces
 
 force_matrix = np.array([[1.5]], dtype=np.float64)
-cut_off = 1.5
+force = force_function(force_matrix=force_matrix, 
+                       positions=position, 
+                       r_c=cut_off, 
+                       types=type_of_particle, 
+                       grid_size=grid_size)
 
-force = force_function(force_matrix=force_matrix, positions=position, r_c=cut_off, types=type_of_particle)
 m = 1.0
 gamma = 1.0
 k_B = 1.0 # Boltzman constant in SI k_B = 1.380649e-23 J/K
@@ -42,8 +49,13 @@ for step in range(steps):
     momentum *= A
     momentum += B*random_fuction(N)
     position += 0.5*momentum/m*dt
-    force = force_function(force_matrix=force_matrix, positions=position, r_c=cut_off, types=type_of_particle)
+    force = force_function(force_matrix=force_matrix, 
+                           positions=position,
+                           r_c=cut_off, 
+                           types=type_of_particle, 
+                           grid_size=grid_size)
     momentum += 0.5*force*dt
+    position = position%grid_size
 
     if step % 2 == 0:
         print(f"t = {step * dt:.2f}")